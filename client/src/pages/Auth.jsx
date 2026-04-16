54951ecba577addbf2a28851a9bc93580773c216
Vinaya <vinayaanil944@gmail.com>
2026-04-16 22:09:18 +0530

    code updated

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Auth() {
+  return (
+    <div className='w-full min-h-screen bg-[black] flex items-center justify-center px-6 py-12 '>
+      Auth page 
+      <div className='w-full max-w-md bg-[white] rounded-lg shadow-lg p-8'>
+
+      </div>
+    </div>
+  )
+}
+
+  
+
+export default Auth
